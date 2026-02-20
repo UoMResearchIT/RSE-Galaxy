d8cadda66aed5e7bd79b653bde3c329cd60540b0
Martin HA <martin.herrerias@gmail.com>
2026-02-20 15:25:27 +0000

    fix: persistent external tool installs
    Applies changes from: https://github.com/UoMResearchIT/Airbus-Wing-of-the-Future/pull/105

--- galaxy/init/template.sh
+++ galaxy/init/template.sh
@@ -9,4 +9,25 @@ for file in /work/*.template; do
   cat "$out"
 done
 
+mkdir -p /galaxy/server/database/config
+
+dynamic_config_files=(
+  integrated_tool_panel.xml
+  migrated_tools_conf.xml
+  shed_data_manager_conf.xml
+  shed_tool_conf.xml
+  shed_tool_data_table_conf.xml
+)
+echo "Bootstrapping dynamic Galaxy config files..."
+for file in "${dynamic_config_files[@]}"; do
+  src="/galaxy/server/config/$file"
+  dst="/galaxy/server/database/config/$file"
+  if [ ! -f "$dst" ]; then
+    echo "Copying $src to $dst"
+    cp "$src" "$dst"
+  else
+    echo "Using existing $dst"
+  fi
+done
+
 chown -R 10001:10001 /galaxy/server/database
